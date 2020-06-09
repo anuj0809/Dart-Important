37969f7c5747bd1f4e81833cfb2339e59dc7935d
Anuj Goyal <53965035+anuj0809@users.noreply.github.com>
2020-06-09 22:49:47 +0530

    Add files via upload

---
+++ maps.dart
@@ -0,0 +1,34 @@
+void main() {
+  var map = {
+    1: 'one',
+    2: 'two'
+  };
+  print(map[2]);
+
+  var demoMap = {
+    '1': 1,
+     3 : 'three'
+  };
+  print(demoMap[3]);
+  print(demoMap['1']);
+  print(demoMap.length);
+
+  map[4] = 'four';
+  print(map[4]);
+
+  var listMap = {
+    1: [1,2,3],
+    2: [4,5,6]
+  };
+  print(listMap[1]);
+  print(listMap[1][2]);
+
+var gifts = Map();
+gifts['first'] = 'partridge';
+gifts['second'] = 'turtledoves';
+gifts['fifth'] = 'golden rings';
+print(gifts);
+}
+	
+\\New comment
+\\checking 
